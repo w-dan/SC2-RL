c66e0ca2e7b0f46ba52d290041c40478216e0ada
w-dan <gdalgora@gmail.com>
2024-02-06 00:15:18 +0100

    game launchs but agent does nothing

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # SC2-RL
+
+!["SC"](https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXdlcDJpcWFkMXo4ZDVmYmN4Nm1mYmJuanAxMDNyeTkxeGF2ejdiMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/4ZkAeUWFF9ZGRXE7Ud/giphy.gif)
+
+RL agent that plays SC2.
+
+## Prerequisites
+- [Starcraft II](https://starcraft2.blizzard.com/en-us/) installed in your system
+- [Python](https://www.python.org/downloads/) >= 3.7
+- [python-sc2](https://github.com/BurnySc2/python-sc2) library
+
+
+## Authors
+- [w-dan](https://github.com/w-dan/)
+- [blitty-codes](https://github.com/blitty-codes/)
+
+Setup created following a [setup tutorial](https://pythonprogramming.net/intro-python-sc2-introduction-starcraft-2-ai/) by [sendtex](https://www.youtube.com/@sentdex)  
---
+++ main.py
@@ -0,0 +1,54 @@
+from sc2.bot_ai import BotAI  # parent class we inherit from
+from sc2.data import Difficulty, Race  # difficulty for bots, race for the 1 of 3 races
+from sc2.main import run_game  # function that facilitates actually running the agents in games
+from sc2.player import Bot, Computer  #wrapper for whether or not the agent is one of your bots, or a "computer" player
+from sc2 import maps  # maps method for loading maps to play in.
+from sc2.ids.unit_typeid import UnitTypeId
+
+MAP_NAME = "Scorpion_1.01"
+
+class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
+    async def on_step(self, iteration: int): # on_step is a method that is called every step of the game.
+        # print(f"{iteration}, n_workers: {self.workers.amount}, n_idle_workers: {self.workers.idle.amount},", \
+        #    f"minerals: {self.minerals}, gas: {self.vespene}, cannons: {self.structures(UnitTypeId.PHOTONCANNON).amount},", \
+        #    f"pylons: {self.structures(UnitTypeId.PYLON).amount}, nexus: {self.structures(UnitTypeId.NEXUS).amount}", \
+        #    f"gateways: {self.structures(UnitTypeId.GATEWAY).amount}, cybernetics cores: {self.structures(UnitTypeId.CYBERNETICSCORE).amount}", \
+        #    f"stargates: {self.structures(UnitTypeId.STARGATE).amount}, voidrays: {self.units(UnitTypeId.VOIDRAY).amount}, supply: {self.supply_used}/{self.supply_cap}")
+        
+
+        print(f"[+] Iteration {iteration}")
+
+        # begin logic:
+        if self.townhalls:
+            nexus = self.townhalls.random
+            if nexus.is_idle and self.can_afford(UnitTypeId.PROBE):  
+                nexus.train(UnitTypeId.PROBE)
+
+
+            # if we dont have *any* pylons, we'll build one close to the nexus.
+            elif not self.structures(UnitTypeId.PYLON) and self.already_pending(UnitTypeId.PYLON) == 0:
+                if self.can_afford(UnitTypeId.PYLON):
+                    await self.build(UnitTypeId.PYLON, near=nexus)
+
+
+            elif self.structures(UnitTypeId.PYLON).amount < 5:
+                if self.can_afford(UnitTypeId.PYLON):
+                    # build from the closest pylon towards the enemy
+                    target_pylon = self.structures(UnitTypeId.PYLON).closest_to(self.enemy_start_locations[0])
+                    # build as far away from target_pylon as possible:
+                    pos = target_pylon.position.towards(self.enemy_start_locations[0], random.randrange(8, 15))
+                    await self.build(UnitTypeId.PYLON, near=pos)
+                    
+
+        else:
+            if self.can_afford(UnitTypeId.NEXUS):  # can we afford one?
+                await self.expand_now()  # build one!
+
+
+
+run_game(  # run_game is a function that runs the game.
+    maps.get(MAP_NAME), # the map we are playing on
+    [Bot(Race.Protoss, IncrediBot()), # runs our coded bot, protoss race, and we pass our bot object 
+     Computer(Race.Zerg, Difficulty.Hard)], # runs a pre-made computer agent, zerg race, with a hard difficulty.
+    realtime=False, # When set to True, the agent is limited in how long each step can take to process.
+)
